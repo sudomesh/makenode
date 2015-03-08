3bb174c268435edaa85d9eaa8d8217bec7e03b75
Marc Juul <juul@sudomesh.org>
2015-03-08 00:40:38 -0800

    minor fixes

--- configs/config.js
+++ configs/config.js
@@ -10,7 +10,16 @@ module.exports = function(u, hwInfo, callback) {
         user_password_hash: u.askPassword("Enter desired admin user password (or press enter to generate)"),
         root_password_hash: u.askPassword("Enter desired root user password (or press enter to generate)"),
         ssh_authorized_keys: u.readFile("authorized_keys"),
+        tx_power: u.askUser("Enter wifi transmit power in dBm"),
 
+        relay_node_inet_ipv4_addr: '104.236.181.226',
+        exit_node_mesh_ipv4_addr: '100.64.0.1',
+
+        operator: {
+            name: u.askUser("Enter operator name or alias"),
+            email: u.askUser("Enter operator email"),
+            phone: null
+        }
     };
 
     if (!u.hasOwnProperty('userConfig')) {
