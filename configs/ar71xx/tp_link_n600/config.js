a014586661c7986cd5a39e4efe0a5219354e6102
Max B <maxb.personal@gmail.com>
2015-05-14 23:28:36 -0700

    Adding tp-link n600 template configs.

---
+++ configs/ar71xx/tp_link_n600/config.js
@@ -0,0 +1,17 @@
+
+
+module.exports = function(u, hwInfo, callback) {
+
+  // this config is only for Western Digital My Net N750 routers
+  if(hwInfo.model != 'TP-LINK TL-WDR3500') {
+    return callback(null, null);
+  }
+
+  var conf = {
+      macAddr: u.macAddr(hwInfo),
+      private_wifi_ssid: u.askUser("Enter desired private wifi SSID"),
+      private_wifi_key: u.askUser("Enter desired private wifi password")
+  };
+
+  callback(null, conf);
+};
