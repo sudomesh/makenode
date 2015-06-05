bf4d49beb42b76df7a2a41f91b5c641284703c5c
Marc Juul <juul@sudomesh.org>
2015-06-05 06:20:15 -0700

    fixed bugs in vlan config and stopped using the semi-reserved VLAN ID 1

--- configs/templates/files/opt/notdhcpserver/hook.sh
+++ configs/templates/files/opt/notdhcpserver/hook.sh
@@ -6,15 +6,18 @@ IP=$3 # The IP that was handed out to the $IFACE
 NETMASK=$4 # The netmask that was handed out to $IFACE
 PASSWORD=$5 # The password that was handed out to $IFACE (on STATE="up" only)
 
+OPEN_IFACE="br-open" # The open bridge interface
 
 case $STATE in
     "up")
-
+        ip route add ${IP}/32 dev $OPEN_IFACE
+        # TODO add interface to babel
         ;;
 
 
     "down")
-
+        ip route del ${IP}/32 dev $OPEN_IFACE
+        # TODO remove interface from babel
         ;;
 
     *)
