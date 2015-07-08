b56355f1d83d663d2e4aed4c3150d8533bbcff26
Marc Juul <juul@sudomesh.org>
2015-07-08 05:53:06 -0700

    all networks (open, mesh and private) now available both on 2.4 and 5 ghz + everything is readied for extending private network with vlan

--- configs/templates/files/opt/notdhcpserver/hook.sh
+++ configs/templates/files/opt/notdhcpserver/hook.sh
@@ -9,6 +9,9 @@ PASSWORD=$5 # The password that was handed out to $IFACE (on STATE="up" only)
 OPEN_IFACE="br-open" # The open bridge interface
 OPEN_VLAN="10" # The VLAN ID of the br-open network
 
+PRIV_IFACE="br-priv" # The private bridge interface
+PRIV_VLAN="11" # The VLAN ID of the br-priv network
+
 VLAN=${IFACE##*.} # The VLAN ID of the receiving interface 
 PORT=$(expr "5" - "$VLAN") # The number of the receiving switch port
 
@@ -22,6 +25,10 @@ case $STATE in
         OLD_PORTS=$(swconfig dev switch0 vlan $OPEN_VLAN get ports)
         swconfig dev switch0 vlan $OPEN_VLAN set ports "$OLD_PORTS ${PORT}t"
 
+        # add the port to the br-priv VLAN (VLAN 11)
+        OLD_PORTS=$(swconfig dev switch0 vlan $PRIV_VLAN get ports)
+        swconfig dev switch0 vlan $PRIV_VLAN set ports "$OLD_PORTS ${PORT}t"
+
         # apply the changes to the switch
         swconfig dev switch0 set apply
 
@@ -32,7 +39,12 @@ case $STATE in
 
     "down")
 
-        # remove the port from the br-open VLAN (VLAN 10)
+        # remove the port from the br-priv VLAN (VLAN 11)
+        OLD_PORTS=$(swconfig dev switch0 vlan $PRIV_VLAN get ports)
+        NEW_PORTS=$(echo $OLD_PORTS | awk -e "{ sub(/ *${PORT}t? */, \" \", \$RESULT); print \$RESULT }")
+        swconfig dev switch0 vlan $PRIV_VLAN set ports "$NEW_PORTS"
+
+        # remove the port from the br-open VLAN (VLAN 10)             
         OLD_PORTS=$(swconfig dev switch0 vlan $OPEN_VLAN get ports)
         NEW_PORTS=$(echo $OLD_PORTS | awk -e "{ sub(/ *${PORT}t? */, \" \", \$RESULT); print \$RESULT }")
         swconfig dev switch0 vlan $OPEN_VLAN set ports "$NEW_PORTS"
