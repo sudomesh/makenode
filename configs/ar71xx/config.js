c497c6a69a6a6f59a7c5c23e47150366fce6f43c
Marc Juul <juul@sudomesh.org>
2015-07-25 22:09:40 -0700

    change meshrouting script to allow notdhcp packets

--- configs/ar71xx/config.js
+++ configs/ar71xx/config.js
@@ -1,22 +1,22 @@
 
 
 module.exports = function(u, hwInfo, callback) {
+    
+    console.log("---------------------------");
+    
+    console.log(hwInfo);
+    
+    console.log("---------------------------");
+    
+    if(u.chipsetType(hwInfo) != 'ar71xx') {
+        return callback(null, null);
+    }
 
-console.log("---------------------------");
-
-console.log(hwInfo);
-
-console.log("---------------------------");
-
-  if(u.chipsetType(hwInfo) != 'ar71xx') {
-    return callback(null, null);
-  }
-
-  var conf = {
-    2_4_channel: 6,
-    5_channel: 157
-  };
-
-        
-  callback(null, conf);
+    var conf = {
+        channel_2_4: 6,
+        channel_5: 157
+    };
+    
+    
+    callback(null, conf);
 };
