fab8885f9dcd6b6639c56f468c7393e1f128157d
Max B <maxb.personal@gmail.com>
2015-09-06 16:11:34 -0700

    Updating mynet n750 dhcpserver hook script.

--- configs/ar71xx/my_net_n750/templates/files/opt/notdhcpserver/hook.sh
+++ configs/ar71xx/my_net_n750/templates/files/opt/notdhcpserver/hook.sh
@@ -7,7 +7,10 @@ NETMASK=$4 # The netmask that was handed out to $IFACE
 PASSWORD=$5 # The password that was handed out to $IFACE (on STATE="up" only)
 
 OPEN_IFACE="br-open" # The open bridge interface
-OPEN_VLAN="7" # The VLAN ID of the br-open network
+OPEN_VLAN="10" # The VLAN ID of the br-open network
+
+PRIV_IFACE="br-priv" # The private bridge interface
+PRIV_VLAN="11" # The VLAN ID of the br-priv network
 
 VLAN=${IFACE##*.} # The VLAN ID of the receiving interface 
 PORT=$VLAN # The number of the receiving switch port
@@ -18,22 +21,32 @@ case $STATE in
         # change the port to be tagged (same VLAN)
         swconfig dev switch0 vlan $VLAN set ports "0t ${PORT}t"
 
-        # add the port to the br-open VLAN (VLAN 7)
+        # add the port to the br-open VLAN (VLAN 10)
         OLD_PORTS=$(swconfig dev switch0 vlan $OPEN_VLAN get ports)
         swconfig dev switch0 vlan $OPEN_VLAN set ports "$OLD_PORTS ${PORT}t"
+
+        # add the port to the br-priv VLAN (VLAN 11)
+        OLD_PORTS=$(swconfig dev switch0 vlan $PRIV_VLAN get ports)
+        swconfig dev switch0 vlan $PRIV_VLAN set ports "$OLD_PORTS ${PORT}t"
 
         # apply the changes to the switch
         swconfig dev switch0 set apply
 
-        # TODO add interface to babel
+        # add interface to babeld
+        babeld -a $IFACE 
         ;;
 
 
     "down")
 
-        # remove the port from the br-open VLAN (VLAN 7)
+        # remove the port from the br-priv VLAN (VLAN 11)
+        OLD_PORTS=$(swconfig dev switch0 vlan $PRIV_VLAN get ports)
+        NEW_PORTS=$(echo $OLD_PORTS | awk -e "{ sub(/ *${PORT}t? */, \" \", \$RESULT); print \$RESULT }")
+        swconfig dev switch0 vlan $PRIV_VLAN set ports "$NEW_PORTS"
+
+        # remove the port from the br-open VLAN (VLAN 10)             
         OLD_PORTS=$(swconfig dev switch0 vlan $OPEN_VLAN get ports)
-        NEW_PORTS=$(echo $OLD_PORTS | awk '{ sub(/ *${PORT}t? */, " ", $RESULT); print $RESULT }')
+        NEW_PORTS=$(echo $OLD_PORTS | awk -e "{ sub(/ *${PORT}t? */, \" \", \$RESULT); print \$RESULT }")
         swconfig dev switch0 vlan $OPEN_VLAN set ports "$NEW_PORTS"
 
         # change the port to be untagged (same VLAN)
@@ -42,7 +55,8 @@ case $STATE in
         # apply the changes to the switch
         swconfig dev switch0 set apply
 
-        # TODO remove interface from babel
+        # remove interface from babeld
+        babeld -x $IFACE 
         ;;
 
     *)
